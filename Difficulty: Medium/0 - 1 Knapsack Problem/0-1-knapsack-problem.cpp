25528ea7a3d055b34f1572178993e4fbaa07847b
SAURABH RAJ <113050183+saurabh-vit@users.noreply.github.com>
2024-12-19 21:20:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return max value that can be put in knapsack of capacity.
+    int knapSack(int capacity, vector<int> &val, vector<int> &wt) {
+        // code here
+        int n = val.size();
+        vector<vector<int>> dp(n+1, vector<int>(capacity+1, 0));
+        for (int i=1; i<=n; i++) {
+            for (int w=1; w<=capacity; w++) {
+                if (wt[i-1] <= w) {
+                    dp[i][w] = max(dp[i-1][w], val[i-1] + dp[i-1][w-wt[i-1]]);
+                } else {
+                    dp[i][w] = dp[i-1][w];
+                }
+            }
+        }
+        return dp[n][capacity];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    // Taking total test cases
+    int testCases;
+    cin >> testCases;
+    cin.ignore();
+    while (testCases--) {
+        // Reading number of items and capacity
+        int numberOfItems, capacity;
+        vector<int> weights, values;
+        string input;
+        int number;
+
+        // Read capacity and number of items
+        getline(cin, input);
+        stringstream ss(input);
+        ss >> capacity;      // The first number is the capacity
+        ss >> numberOfItems; // The second number is the number of items
+
+        // Read values
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            values.push_back(number);
+        }
+
+        // Read weights
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            weights.push_back(number);
+        }
+
+        Solution solution;
+        cout << solution.knapSack(capacity, values, weights) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
