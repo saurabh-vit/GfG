4e66a018b7cf0c102e6d2f6581a5900d5a9e21fd
SAURABH RAJ <113050183+saurabh-vit@users.noreply.github.com>
2025-06-07 21:56:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
+++ Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
@@ -1,70 +1,38 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-// User function Template for C++
-
-class Solution{
+class Solution {
 public:
-    void solveNQueensHelper(int n, int row, vector<int>& board, set<int>& cols, set<int>& diagonals1, set<int>& diagonals2, vector<vector<int>>& results) {
-        if (row == n) {
-            results.push_back(board);
+    vector<vector<int>> ans;
+
+    bool issafe(vector<int>& board, int col, int row) {
+        for (int prevCol = 0; prevCol < col; prevCol++) {
+            int prevRow = board[prevCol];
+            // Check same row
+            if (prevRow == row) return false;
+            // Check diagonals
+            if (abs(prevRow - row) == abs(prevCol - col)) return false;
+        }
+        return true;
+    }
+
+    void solve(int col, int n, vector<int>& board) {
+        if (col == n) {
+            vector<int> res = board;
+            for (int& r : res) r++; // convert 0-based to 1-based
+            ans.push_back(res);
             return;
         }
-        for (int col = 0; col < n; col++) {
-            if (cols.count(col) || diagonals1.count(row - col) || diagonals2.count(row + col)) {
-                continue;
+
+        for (int row = 0; row < n; row++) {
+            if (issafe(board, col, row)) {
+                board[col] = row;
+                solve(col + 1, n, board);
+                // backtrack not needed here as we're overwriting board[col]
             }
-            board[row] = col + 1;
-            cols.insert(col);
-            diagonals1.insert(row - col);
-            diagonals2.insert(row + col);
-            solveNQueensHelper(n, row + 1, board, cols, diagonals1, diagonals2, results);
-            board[row] = 0;
-            cols.erase(col);
-            diagonals1.erase(row - col);
-            diagonals2.erase(row + col);
         }
     }
+
     vector<vector<int>> nQueen(int n) {
-        vector<vector<int>> results;
-        vector<int> board(n, 0);
-        set<int> cols;
-        set<int> diagonals1;
-        set<int> diagonals2;
-        solveNQueensHelper(n, 0, board, cols, diagonals1, diagonals2, results);
-        return results;
+        vector<int> board(n, -1); // board[col] = row
+        solve(0, n, board);
+        return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        
-        Solution ob;
-        vector<vector<int>> ans = ob.nQueen(n);
-        if(ans.size() == 0)
-            cout<<-1<<"\n";
-        else {
-            for(int i = 0;i < ans.size();i++){
-                cout<<"[";
-                for(int u: ans[i])
-                    cout<<u<<" ";
-                cout<<"] ";
-            }
-            cout<<endl;
-        }
-    
-cout << "~" << "\n";
-}
-    return 0;
-}
-// } Driver Code Ends
