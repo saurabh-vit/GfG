b557f322a5c6242f63fc5f729534a835e9f0854a
SAURABH RAJ <113050183+saurabh-vit@users.noreply.github.com>
2025-08-09 20:50:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Longest Periodic Proper Prefix/longest-periodic-proper-prefix.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    int getLongestPrefix(string &s) {
+        // code here
+        int n = s.size();
+        vector<int> z(n, 0);
+
+        // Compute Z-function
+        for (int i = 1, l = 0, r = 0; i < n; i++) {
+            if (i <= r)
+                z[i] = min(r - i + 1, z[i - l]);
+            while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+                z[i]++;
+            if (i + z[i] - 1 > r)
+                l = i, r = i + z[i] - 1;
+        }
+
+        // Try largest proper prefix length
+        for (int lenP = n - 1; lenP >= 1; lenP--) {
+            // If the substring after lenP matches prefix for the rest of s
+            if (z[lenP] >= n - lenP) {
+                return lenP;
+            }
+        }
+        return -1;
+    }
+};
