6b65bd6cd493982ee1c224550b9d43e61a36bbb9
SAURABH RAJ <113050183+saurabh-vit@users.noreply.github.com>
2026-01-02 19:16:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
@@ -1,46 +1,23 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
     void sort012(vector<int>& arr) {
         // code here
-        sort(arr.begin(), arr.end());
+        int low = 0, mid = 0;
+        int high = arr.size() - 1;
+
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                swap(arr[low], arr[mid]);
+                low++;
+                mid++;
+            }
+            else if (arr[mid] == 1) {
+                mid++;
+            }
+            else { // arr[mid] == 2
+                swap(arr[mid], arr[high]);
+                high--;
+            }
+        }
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-
-        vector<int> a;
-        string input;
-        int num;
-
-        getline(cin, input);
-        stringstream s2(input);
-        while (s2 >> num) {
-            a.push_back(num);
-        }
-        Solution ob;
-        ob.sort012(a);
-
-        int n = a.size();
-        for (int i = 0; i < n; i++) {
-            cout << a[i] << " ";
-        }
-
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
