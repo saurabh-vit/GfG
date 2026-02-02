209d7ff61290495430af2ed53795a07429137755
SAURABH RAJ <113050183+saurabh-vit@users.noreply.github.com>
2026-02-02 14:33:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.cpp
+++ Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.cpp
@@ -3,23 +3,23 @@ class Solution {
     int maxCircularSum(vector<int> &arr) {
         // code here
         int n = arr.size();
-        int curr_max = arr[0], max_kadane = arr[0];
+        int curr = arr[0], maxi = arr[0];
         for(int i=1; i<n; i++){
-            curr_max = max(arr[i], curr_max + arr[i]);
-            max_kadane = max(max_kadane, curr_max);
+            curr = max(arr[i], curr + arr[i]);
+            maxi = max(maxi, curr);
         }
-        int total_sum = 0;
+        int sum = 0;
         int curr_min = arr[0], min_kadane = arr[0];
         for(int i=0; i<n; i++){
-            total_sum += arr[i];
+            sum += arr[i];
             if(i != 0) {
                 curr_min = min(arr[i], curr_min + arr[i]);
                 min_kadane = min(min_kadane, curr_min);
             }
         }
-        if(max_kadane < 0) {
-            return max_kadane;
+        if(maxi < 0) {
+            return maxi;
         }
-        return max(max_kadane, total_sum - min_kadane);
+        return max(maxi, sum - min_kadane);
     }
 };
