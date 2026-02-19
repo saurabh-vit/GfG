2a4466f6a1872bd61c4aa5b3b4b90bcaf193872c
SAURABH RAJ <113050183+saurabh-vit@users.noreply.github.com>
2026-02-20 02:57:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Form the Largest Number/form-the-largest-number.cpp
+++ Difficulty: Medium/Form the Largest Number/form-the-largest-number.cpp
@@ -2,21 +2,26 @@ class Solution {
   public:
     string findLargest(vector<int> &arr) {
         // code here
-        vector<string> ans;
-        for(int x : arr){
-            ans.push_back(to_string(x));
+        vector<string> nums;
+        for(int num : arr){
+            nums.push_back(to_string(num));
         }
         
-        sort(ans.begin(), ans.end(), [](string &a, string &b){
-            return a+b > b+a;
+        // Step 2: Custom sort
+        sort(nums.begin(), nums.end(), [](string &a, string &b){
+            return a + b > b + a;
         });
         
-        if(ans[0] == "0") return "0";
+        // Step 3: Edge case (all zeros)
+        if(nums[0] == "0")
+            return "0";
         
+        // Step 4: Concatenate result
         string result = "";
-        for(string &s : ans){
+        for(string &s : nums){
             result += s;
         }
+        
         return result;
     }
 };
