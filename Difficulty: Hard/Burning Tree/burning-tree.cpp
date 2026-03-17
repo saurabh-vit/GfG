9f7cfd2be0b295a1ab8008b3c3cc83fe1566f9a0
SAURABH RAJ <113050183+saurabh-vit@users.noreply.github.com>
2026-03-17 21:02:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Burning Tree/burning-tree.cpp
@@ -0,0 +1,98 @@
+/*
+class Node {
+  public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+  
+    // Step 1: Build parent mapping + find target
+    Node* markParents(Node* root, unordered_map<Node*, Node*>& parent, int target) {
+        
+        queue<Node*> q;
+        q.push(root);
+        
+        Node* targetNode = NULL;
+        
+        while(!q.empty()) {
+            Node* curr = q.front();
+            q.pop();
+            
+            if(curr->data == target)
+                targetNode = curr;
+            
+            if(curr->left) {
+                parent[curr->left] = curr;
+                q.push(curr->left);
+            }
+            
+            if(curr->right) {
+                parent[curr->right] = curr;
+                q.push(curr->right);
+            }
+        }
+        
+        return targetNode;
+    }
+
+    int minTime(Node* root, int target) {
+        
+        unordered_map<Node*, Node*> parent;
+        
+        // get target node
+        Node* targetNode = markParents(root, parent, target);
+        
+        // BFS
+        queue<Node*> q;
+        unordered_map<Node*, bool> visited;
+        
+        q.push(targetNode);
+        visited[targetNode] = true;
+        
+        int time = 0;
+        
+        while(!q.empty()) {
+            int size = q.size();
+            bool burned = false;
+            
+            for(int i = 0; i < size; i++) {
+                Node* curr = q.front();
+                q.pop();
+                
+                // left
+                if(curr->left && !visited[curr->left]) {
+                    burned = true;
+                    visited[curr->left] = true;
+                    q.push(curr->left);
+                }
+                
+                // right
+                if(curr->right && !visited[curr->right]) {
+                    burned = true;
+                    visited[curr->right] = true;
+                    q.push(curr->right);
+                }
+                
+                // parent
+                if(parent[curr] && !visited[parent[curr]]) {
+                    burned = true;
+                    visited[parent[curr]] = true;
+                    q.push(parent[curr]);
+                }
+            }
+            
+            if(burned) time++;
+        }
+        
+        return time;
+    }
+};
