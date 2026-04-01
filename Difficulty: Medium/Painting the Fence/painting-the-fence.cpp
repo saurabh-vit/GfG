a83ea03c303378593ed94b3ceb181577f06b503e
SAURABH RAJ <rajsaurabh408@gmail.com>
2026-04-02 01:38:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Painting the Fence/painting-the-fence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int countWays(int n, int k) {
+        // code here
+        if (n == 1) return k;
+
+        int same = k;                // for n = 2
+        int diff = k * (k - 1);      // for n = 2
+
+        for (int i = 3; i <= n; i++) {
+            int newSame = diff;
+            int newDiff = (same + diff) * (k - 1);
+
+            same = newSame;
+            diff = newDiff;
+        }
+
+        return same + diff;
+    }
+};
