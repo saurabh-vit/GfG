990202ea1cc2296df773e20347bed9ac1a2061d4
SAURABH RAJ <rajsaurabh408@gmail.com>
2026-06-23 10:32:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse an Array/reverse-an-array.cpp
+++ Difficulty: Easy/Reverse an Array/reverse-an-array.cpp
@@ -3,11 +3,11 @@ class Solution {
     void reverseArray(vector<int> &arr) {
         // code here
         int n = arr.size();
-        int st = 0, end = n-1;
-        while(st<=end){
-            swap(arr[st], arr[end]);
-            st++;
-            end--;
+        int l = 0, r = n-1;
+        while(l<=r){
+            swap(arr[l], arr[r]);
+            l++;
+            r--;
         }
     }
 };
