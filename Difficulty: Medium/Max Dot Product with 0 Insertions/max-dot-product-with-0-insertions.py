2dec758d3cff30bf453870698a6bfec06369d5f6
SAURABH RAJ <rajsaurabh408@gmail.com>
2026-06-29 19:37:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Dot Product with 0 Insertions/max-dot-product-with-0-insertions.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxDotProduct(self, a, b):
+        # code here
+        n=len(a)
+        m=len(b)
+        NEG = float('-inf')
+
+        dp = [[NEG] * (m + 1) for _ in range(n + 1)]
+
+        for i in range(n + 1):
+            dp[i][0] = 0
+
+        for i in range(1, n + 1):
+            for j in range(1, min(i, m) + 1):
+                dp[i][j] = max(
+                    dp[i - 1][j],
+                    dp[i - 1][j - 1] + a[i - 1] * b[j - 1]
+                )
+
+        return dp[n][m]
