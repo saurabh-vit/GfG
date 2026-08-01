88bd06ea55adc9abf324c65d85f1d378ae280c22
SAURABH RAJ <rajsaurabh408@gmail.com>
2026-08-01 23:00:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max After m Range Increments/max-after-m-range-increments.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int findMax(int n, vector<int>& a, vector<int>& b, vector<int>& k) {
+        // code here
+        vector<long long> diff(n + 1, 0);
+
+        int m = a.size();
+
+        for (int i = 0; i < m; i++) {
+            diff[a[i]] += k[i];
+            if (b[i] + 1 < n)
+                diff[b[i] + 1] -= k[i];
+        }
+
+        long long cur = 0, ans = 0;
+        for (int i = 0; i < n; i++) {
+            cur += diff[i];
+            ans = max(ans, cur);
+        }
+
+        return ans;
+    }
+};
