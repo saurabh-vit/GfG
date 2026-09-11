cd67e26f6ff498a1f98dec3fa156d2db277c431f
SAURABH RAJ <rajsaurabh408@gmail.com>
2026-09-11 23:10:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Values with Equal Array Remainders/values-with-equal-array-remainders.cpp
+++ Difficulty: Easy/Values with Equal Array Remainders/values-with-equal-array-remainders.cpp
@@ -1,29 +1,25 @@
 class Solution {
   public:
     int sameMod(vector<int> &arr) {
+        // code here
         int g = 0;
-
-        // Find GCD of differences
-        for (int i = 1; i < arr.size(); i++) {
+        for(int i = 1; i<arr.size(); i++){
             g = gcd(g, abs(arr[i] - arr[0]));
         }
-
-        // All elements are equal
-        if (g == 0)
+        
+        if(g == 0){
             return -1;
-
-        // Count divisors of GCD
+        }
+        
         int count = 0;
-
-        for (int i = 1; i * i <= g; i++) {
-            if (g % i == 0) {
+        for(int i = 1; i*i <= g; i++){
+            if(g%i==0){
                 count++;
-
-                if (i != g / i)
+                if(i != g/i){
                     count++;
+                }
             }
         }
-
         return count;
     }
 };
